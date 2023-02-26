88d1994d073c4dda1b6c51dd380b648f21f29a02
Dzung <dungtangtri@gmail.com>
2023-02-26 16:47:23 +0700

    Fixing bugs

--- server/routes/plants.js
+++ server/routes/plants.js
@@ -21,7 +21,7 @@ router.get('/search', async (req, res) => {
   const searchResults = await searchPlants(searchTerm);
   if (searchResults == undefined) {
     res.redirect('/');
-  }else if(searchResults[0] == null){
+  } else if (searchResults[0] == null) {
     res.send("No results were found!")
   }
   else {
@@ -46,14 +46,39 @@ router.get('/search', async (req, res) => {
         gia_tri: searchResults[0]['Giá trị'],
         tinh_trang_bao_ton: searchResults[0]['Tình trạng bảo tồn, kinh doanh'],
         tai_lieu_dan: searchResults[0]['Tài liệu dẫn'],
+        name: req.user.username,
+        status: true
       };
-      
+
       res.render('search-result', { tree: tree });
       console.log(req.query);
 
     } catch (error) {
       console.log(error);
-      res.send("Error, please try again!");
+      let tree = {
+        id: searchResults[0]['ID'],
+        ten: searchResults[0]['Name'],
+        dang_song: searchResults[0]['Dạng sống'],
+        cong_dung: searchResults[0]['Công dụng'],
+        sach_do: searchResults[0]['Sách đỏ'],
+        ten_bo: searchResults[0]['TÊN BỘ'],
+        ten_bo1: searchResults[0]['TÊN BỘ1'],
+        ten_ho: searchResults[0]['TÊN HỌ'],
+        ten_ho1: searchResults[0]['TÊN HỌ1'],
+        ten_chi: searchResults[0]['TÊN CHI'],
+        ten_chi1: searchResults[0]['TÊN CHI1'],
+        ten_loai: searchResults[0]['TÊN LOÀI'],
+        dong_danh: searchResults[0]['Đồng danh'],
+        dac_diem_hinh_thai: searchResults[0]['Đặc điểm hình thái'],
+        dac_diem_sinh_hoc: searchResults[0]['Đặc điểm sinh học và hình thái học'],
+        phan_bo_dia_ly: searchResults[0]['Phân bố địa lý'],
+        gia_tri: searchResults[0]['Giá trị'],
+        tinh_trang_bao_ton: searchResults[0]['Tình trạng bảo tồn, kinh doanh'],
+        tai_lieu_dan: searchResults[0]['Tài liệu dẫn'],
+        name: "user",
+        status: false
+      };
+      res.render('search-result', { tree: tree});
     }
   }
 });
